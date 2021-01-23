179ba350253d6ed63d8d6812a84602c96780d627
AWS <wahab305@gmail.com>
2021-01-23 00:39:54 -0500

    changed greeting

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send({"Heroku is amazing"});
+    res.send({ 'Heroku is amazing' });
 });
 
 const PORT = process.env.PORT || 5000;
